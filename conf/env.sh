bf7f7c71d513066fd483edb0058eaed872d0ce39
Junjie Chen <junjie.j.chen@intel.com>
2016-11-16 10:18:08 +0800

    Add custom property files

--- conf/env.sh
+++ conf/env.sh
@@ -3,3 +3,6 @@
 # the path to the tool configuration
 export SLAVES_CONF_NAME="slaves.property"
 export CORE_SITE_CONF_NAME="core-site.property"
+export HDFS_SITE_CONF_NAME="hdfs-site.property"
+export YARN_SITE_CONF_NAME="yarn-site.property"
+export MAPRED_SITE_CONF_NAME="mapred-site.property"
