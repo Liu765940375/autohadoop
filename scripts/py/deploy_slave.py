2163fcdef902db32bcd7fb2f24cfa5b42eed4c2b
Junjie Chen <junjie.j.chen@intel.com>
2016-11-17 14:53:21 +0800

    copy packages and configuration to slave nodes

--- scripts/py/deploy_slave.py
+++ scripts/py/deploy_slave.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 
-import sys
 import os
 import paramiko
+import glob
 import xml.etree.cElementTree as ET
+
+class Node:
+    def __init__(self, hostname, ip, username, password):
+        self.hostname = hostname
+        self.ip = ip
+        self.username = username
+        self.password = password
 
 def get_custom_configs (filename, custom_configs):
     with open(filename) as f:
@@ -11,21 +18,21 @@ def get_custom_configs (filename, custom_configs):
             key, value = line.partition("=")[::2]
             custom_configs[key.strip()] = value.strip()
 
-def ssh_execute(server, user, psw, cmd):
+def ssh_execute(node,  cmd):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.load_system_host_keys()
-    ssh.connect(server, username=user, password=psw)
+    ssh.connect(node.ip, username=node.username,password=node.password)
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
     for line in ssh_stdout:
         print '...' + line.strip('\n')
     ssh.close()
 
-def ssh_copy(server, user, psw, src, dst):
+def ssh_copy(node, src, dst):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.load_system_host_keys()
-    ssh.connect(server, username=user, password=psw)
+    ssh.connect(node.ip, username=node.username, password=node.password)
     sftp = ssh.open_sftp()
     sftp.put(src, dst)
     sftp.close()
@@ -55,8 +62,11 @@ def get_slaves(filename):
         return slaves
     with open(filename) as f:
         for line in f:
-            slave = line.split()
-            slaves.append(slave)
+            val = line.split()
+            if len(val) != 4:
+                print "Wrong format of slave config"
+            node = Node(val[0], val[1], val[2], val[3])
+            slaves.append(node)
 
     return slaves
 
@@ -108,31 +118,49 @@ def generate_configuration(config_template_file, custom_config_file, target_conf
     with open(target_config_file, "w") as f:
         tree.write(f)
 
-
-
 if __name__ == '__main__':
     current_path = os.path.dirname(os.path.abspath(__file__))
     script_path = os.path.dirname(current_path)
     project_path = os.path.dirname(script_path)
-    config_path = project_path + "/conf/"
-    config_files = ["hdfs-site.xml", "core-site.xml", "mapred-site.xml", "yarn-site.xml"]
+    config_path = project_path + "/conf"
+    package_path = project_path +"/packages"
+    config_file_names = ["hdfs-site.xml", "core-site.xml", "mapred-site.xml", "yarn-site.xml"]
 
     # Generate configration XML files
-    for config_file in config_files:
-        template_config = config_path + config_file + ".template"
-        custom_config = config_path + config_file + ".custom"
-        target_config = config_path + config_file
+    for config_file in config_file_names:
+        template_config = os.path.join(config_path, config_file) + ".template"
+        custom_config = os.path.join(config_path, config_file) + ".custom"
+        target_config = os.path.join(config_path, config_file)
         generate_configuration(template_config, custom_config, target_config)
 
     # Copy hadoop package to slave nodes
-    slaves = get_slaves(config_path + "slaves")
+    slaves = get_slaves(os.path.join(config_path, "slaves"))
+    path = package_path + "/*.tar.gz"
+    packages = glob.glob(path)
+
+    path = config_path + "/*"
+    final_config_files = glob.glob(path)
+
+    path = package_path + "/hadoop*.tar.gz"
 
     # Get namenode and datanode dir
-    datanode_dir = get_config(config_path + "hdfs-site.xml", "dfs.namenode.name.dir").split(':')[1]
-    namenode_dir = get_config(config_path + "hdfs-site.xml", "dfs.datanode.data.dir").split(':')[1]
+    datanode_dir = get_config(os.path.join(config_path, "hdfs-site.xml"), "dfs.namenode.name.dir").split(':')[1]
+    namenode_dir = get_config(os.path.join(config_path, "hdfs-site.xml"), "dfs.datanode.data.dir").split(':')[1]
     cmd = "mkdir -p " + namenode_dir + ";"
     cmd += "mkdir -p " + datanode_dir + ";"
 
-    for slave in slaves:
-        print "deploy " + slave[0]
-        ssh_execute(slave[1], slave[2], slave[3], cmd)
+    for node in slaves:
+        print "Start deploying" + node.hostname
+        print "Copy hadoop packages"
+        for file in packages:
+            ssh_execute(node, "mkdir -p /opt")
+            ssh_copy(node, file, "/opt/"+os.path.basename(file))
+            ssh_execute(node, "cd /opt/; mkdir -p hadoop" )
+            ssh_execute(node, "cd /opt/; tar zxf " + os.path.basename(file) + "-C /opt/hadoop --strip-components=1")
+
+        print "Copying hadoop configuration files..."
+        for file in final_config_files:
+            ssh_copy(node, file, "/opt/hadoop/etc/hadoop/" + os.path.basename(file))
+
+        print "Create directory for namenode and datanode dir..."
+        ssh_execute(node, cmd)
