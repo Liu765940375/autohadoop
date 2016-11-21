3ec3b9eddf6204a44bfbd04bb9d991ac79e939fc
Junjie Chen <junjie.j.chen@intel.com>
2016-11-21 12:54:04 +0800

    Append environment variable to component resource config instead of .bashrc, such as /opt/hadooprc, and then source it in .bashrc.

--- scripts/py/deploy_slave.py
+++ scripts/py/deploy_slave.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import os
-import sys
 import paramiko
 import glob
 import optparse
@@ -69,33 +68,35 @@ def get_slaves(filename):
             val = line.split()
             if len(val) != 4:
                 print "Wrong format of slave config"
-            node = Node(val[0], val[1], val[2], val[3])
-            slaves.append(node)
+                break
+            else:
+                node = Node(val[0], val[1], val[2], val[3])
+                slaves.append(node)
 
     return slaves
 
 def setup_config_dist(slaves, config_files, component):
-    print "Distribute config xml for " + component
+    print "distribute config xml for" + component
     if component == "hadoop":
         for file in config_files:
             for node in slaves:
                 ssh_copy(node, file, "/opt/hadoop/etc/hadoop/" + os.path.basename(file))
 
-def download_package_dist(slaves, file, component):
-    print "distrubte package for " + component
+# Copy and unpack a package to slave nodes
+def copy_package_dist(slaves, file, component):
+    print "Distrubte package for " + component
     for node in slaves:
         ssh_execute(node, "mkdir -p /opt")
         ssh_copy(node, file, "/opt/" + os.path.basename(file))
-        ssh_execute(node, "cd /opt/; mkdir -p " + component)
-        cmd = "tar zxf /opt/" + os.path.basename(file) +\
+        cmd = "mkdir -p /opt/" + component + ";tar zxf /opt/" + os.path.basename(file) +\
               " -C /opt/" + component + " --strip-components=1"
         ssh_execute(node, cmd)
 
+# Execute command on slave nodes
 def execute_command_dist(slaves, command, component):
     print "Execute commands over slaves"
     for node in slaves:
         ssh_execute(node, command)
-
 
 def get_env_list(filename):
     envs = {}
@@ -111,20 +112,22 @@ def get_env_list(filename):
 
     return envs
 
-def setup_env_dist(slaves, envs):
+def setup_env_dist(slaves, envs, component):
     print "Setup Environment over slaves"
 
     for node in slaves:
         for key, value in envs.iteritems():
-            cmd = "export " + key + "=" + value
-            ssh_execute(node, "echo \"" + cmd + "\" >> ~/.bashrc")
+            cmd = "echo \"export " + key + "=" + value + "\"> /opt" + component + "rc";
+            cmd += "echo \". /opt/hadooprc\" >> ~/.bashrc"
+            ssh_execute(node, cmd)
+
 
 def generate_configuration(config_template_file, custom_config_file, target_config_file):
     default_configs = {}
     custom_configs = {}
 
     configs = ET.parse(config_template_file)
-    root = configs.getroot();
+    root = configs.getroot()
 
     # No custom file exsit
     if not os.path.isfile(custom_config_file):
@@ -175,7 +178,7 @@ def setup_nopass(slaves):
 
     for node in slaves:
         ssh_copy(node, rsa_file, "/tmp/id_rsa.pub")
-        ssh_execute(node, "cat /tmp/id_rsa.pub > ~/.ssh/authorized_keys")
+        ssh_execute(node, "cat /tmp/id_rsa.pub >> ~/.ssh/authorized_keys")
         ssh_execute(node, "chmod 0600 ~/.ssh/authorized_keys")
 
 if __name__ == '__main__':
@@ -196,37 +199,35 @@ if __name__ == '__main__':
     package_path = project_path +"/packages"
     config_file_names = ["hdfs-site.xml", "core-site.xml", "mapred-site.xml", "yarn-site.xml"]
 
-    # Distribute component package to slave nodes
-    slaves = get_slaves(os.path.join(config_path, "slaves.property"))
-    with open(os.path.join(config_path, "slaves"), "w") as f:
-        for node in slaves:
-            f.write(node.ip + "\n")
+    # Setup Nopass for slave nodes
+    slaves = get_slaves(os.path.join(config_path, "slaves"))
     setup_nopass(slaves)
 
-    # setup ENV
+    # Setup ENV on slave nodes
     envs = get_env_list(os.path.join(config_path, "env"))
     setup_env_dist(slaves, envs)
 
-    # Download component package
+    # Copy component package to slave nodes
     download_server = "10.239.47.53"
     package = component + "-" + version + ".tar.gz";
     if not os.path.isfile(os.path.join(package_path, package)):
         download_url = "http://" + download_server + "/" + component
         os.system("wget -P " + package_path + " " + download_url + "/" + package)
+    copy_package_dist(slaves, os.path.join(package_path, package), component)
 
-    # Download and install JDK
+    # Copy JDK
+    # TODO: It should only setup one time.
     jdk_version = envs["JDK_VERSION"]
     package = "jdk-" + jdk_version + "-linux-x64.tar.gz";
     if not os.path.isfile(os.path.join(package_path, package)):
         download_url = "http://" + download_server + "/software"
         os.system("wget -P " + package_path + " " + download_url + "/" + package)
+    copy_package_dist(slaves, os.path.join(package_path, package), "jdk")
 
-    # TODO: only copy component package or all?
-    path = package_path + "/*.tar.gz"
-    packages = glob.glob(path)
-    for pkg in packages:
-        module = os.path.basename(pkg).split('-')[0]
-        download_package_dist(slaves, pkg, module)
+    # Add binary path to PATH
+    for node in slaves:
+        cmd="echo \"export PATH=$PATH:$JAVA_HOME/bin:$HADOOP_HOME/bin\" >> /opt/" + component +"rc"
+        ssh_execute(node, cmd)
 
     # Generate configration XML files
     for config_file in config_file_names:
@@ -248,7 +249,3 @@ if __name__ == '__main__':
         cmd += "mkdir -p " + datanode_dir + ";"
     execute_command_dist(slaves, cmd, component)
 
-    # PATH
-    for node in slaves:
-        cmd="echo \"export PATH=$PATH:$JAVA_HOME/bin:$HADOOP_HOME/bin\" >> ~/.bashrc"
-        ssh_execute(node, cmd)
