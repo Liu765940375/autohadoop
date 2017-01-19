9c489ad78790ab051ce4dc1b4081c744c0cc857a
Ferdinand Xu <xuf@apache.org>
2017-01-19 15:45:01 +0800

    Update utils.py

--- distribute/utils.py
+++ distribute/utils.py
@@ -137,7 +137,7 @@ def copy_package_dist(slaves, file, component, version):
 
 # Copy component package to slave nodes
 def copy_packages(component, version, slaves):
-    print colors.LIGHT_GREEN + "Distrubte " + "tar.gz file" + " for " + component + colors.ENDC
+    print colors.LIGHT_GREEN + "Distribute " + "tar.gz file" + " for " + component + colors.ENDC
     download_url = "http://" + download_server + "/" + component
     package = component + "-" + version + ".tar.gz"
     if not os.path.isfile(os.path.join(package_path, package)):
